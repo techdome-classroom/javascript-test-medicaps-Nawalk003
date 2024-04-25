70aa8b541cc25cd07ff1e2d9196f20b11786df0d
Nawal Kumar <98410415+Nawalk003@users.noreply.github.com>
2024-04-25 13:53:02 +0530

    Update program2.js

--- program2.js
+++ program2.js
@@ -1,7 +1,22 @@
 function smallestMissingPositiveInteger(nums) {
+    if (nums.length === 0) return 1;
+  if (nums.length === 1) return nums[0] <= 0 ? 1 : 2;
 
-  // Implement the function smallest_missing_positive_integer
+  let n = nums.length;
+  for (let i = 0; i < n; i++) {
+    
+    while (0 < nums[i] <= n && nums[i] != nums[nums[i] - 1]) {
+      let j = nums[i] - 1;
+      [nums[i], nums[j]] = [nums[j], nums[i]];
+    }
+  }
 
+  for (let i = 0; i < n; i++) {
+    if (nums[i] != i + 1) {
+      return i + 1;
+    }
+  }
+  return n + 1;
 }
 
 module.exports = smallestMissingPositiveInteger;
